4b901ca036acc586094403413ef4619308edea94
dconbal <dconbal598@g.educaand.es>
2021-10-05 13:30:45 +0200

    edicion

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/src/app.dart';
 
+//Esto es el que ejecuta la aplicacion
 void main() {
   //defile a flutter que corra la aplicacion
   runApp(MyApp());
